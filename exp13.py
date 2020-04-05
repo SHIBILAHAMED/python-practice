04a931a7147445dbcb7cc780732a859cf55a7603
Shibil Ahamed <shibilahamedpk@nowhere.in>
2020-04-05 20:01:49 +0530

    understan argument

---
+++ exp13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print ("the script is called:", script)
+print ("your first varible is :",first)
+print ("your second varible is :",second)
+print ("your third varible is:",third)
