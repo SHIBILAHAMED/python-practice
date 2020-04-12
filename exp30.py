e9e562b8d96a12da0ace8473d8d49b5fad894a0a
Shibil Ahamed <shibilahamedpk@nowhere.in>
2020-04-12 10:58:45 +0530

    else if completed

---
+++ exp30.py
@@ -0,0 +1,26 @@
+people= 30
+cars=40
+trucks =15
+
+if cars > people:
+    print ("we shouljd tke the cars.")
+
+elif cars< people:
+    print("we should not take the cars.")
+
+else:
+    print ("we cnt decide.")
+
+if trucks> cars:
+    print("that's too many trucks .")
+elif trucks <cars:
+    print("maybe we could take the trucks .")
+
+else:
+    print("we still cant decide")
+
+if people >trucks:
+    print ("alright lets just take the trucks.")
+
+else:
+    print("fine,lets stay home then.")
