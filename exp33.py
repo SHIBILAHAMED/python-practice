fc2ae1d718eeeef1830e0a24281fb153b30db32c
Shibil Ahamed <shibilahamedpk@nowhere.in>
2020-04-13 07:54:34 +0530

    completed

---
+++ exp33.py
@@ -0,0 +1,17 @@
+i =0
+numbers =[]
+while i<6 :
+    print (f"all the top i is {i}")
+    numbers.append(i)
+
+    i+=1
+    print ("numbers now",numbers)
+    print (f"at the bottom i is {i}")
+
+print ("the number ")
+#for z in numbers:
+while y <numbers:
+    
+    print ("hai",y)
+    y+=1
+ 
